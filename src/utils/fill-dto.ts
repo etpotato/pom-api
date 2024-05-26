9c76df2c79d58727fc03ddc7c942ef159ae56f01
Pavel Mareev <mareev.pv@gmail.com>
2024-05-26 17:36:36 +0200

    feat: fill-dto util

---
+++ src/utils/fill-dto.ts
@@ -0,0 +1,10 @@
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+
+export function fillDto<T extends object>(
+  source: object,
+  dto: ClassConstructor<T>,
+) {
+  return plainToInstance(dto, source, {
+    excludeExtraneousValues: true,
+  });
+}
