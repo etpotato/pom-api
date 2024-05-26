22d8b4bfe92fbe253f72b484a2a6b2e73f84985e
Pavel Mareev <mareev.pv@gmail.com>
2024-05-26 11:01:54 +0200

    refactor: remove database module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AppConfigModule } from '../config';
-import { DatabaseModule } from '../database';
-import { SourceModule } from '../source';
+import { AppConfigModule, AppConfigService } from '../config';
+import { Source, SourceModule } from '../source';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, SourceModule],
+  imports: [
+    AppConfigModule,
+    SourceModule,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory(config: AppConfigService) {
+        return {
+          type: 'postgres',
+          host: config.get('DB_HOST'),
+          port: config.get('DB_PORT'),
+          username: config.get('DB_USERNAME'),
+          password: config.get('DB_PASSWORD'),
+          database: config.get('DB_NAME'),
+          entities: [Source],
+        };
+      },
+      inject: [AppConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/source/index.ts
+++ src/modules/source/index.ts
@@ -1,1 +1,2 @@
 export * from './source.module';
+export * from './source.entity';
